baf28ac49a50b7a68b13e689a7852abc65028f6c
Cooper Davis <codavis@hmc.edu>
2026-04-14 11:55:15 -0700

    Phake Lake Day
    tank room data + waypoints for phake lake

--- libraries/main/DepthControl.h
+++ libraries/main/DepthControl.h
@@ -35,7 +35,7 @@ public:
   float depth_des;   // desired depth
   float depth;       // current depth
   float depth_error; // distance to waypoint
-  float Kp= 250.0;     // proportional control gain
+  float Kp = 250.0;     // proportional control gain
   float uV;          // vertical motor effort
 
   bool diveState = 1;
