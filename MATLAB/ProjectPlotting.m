e02f3efcf4e7f9e0cdb985fb71600ab093717afa
kt-kch <kacheng@g.hmc.edu>
2026-05-03 23:27:08 -0700

    Final Plotting Code

--- MATLAB/ProjectPlotting.m
+++ MATLAB/ProjectPlotting.m
@@ -4,7 +4,7 @@
 clear;
 %clf;
 
-filenum = '_final'; % file number for the data you want to read
+filenum = '012'; % file number for the data you want to read
 infofile = strcat('INF', filenum, '.TXT');
 datafile = strcat('LOG', filenum, '.BIN');
 
@@ -47,7 +47,7 @@ fclose(fid);
 
 %% Process your data here
 
-%% assigned channels based on logger mapping -> Edit based on column names 
+% assigned channels based on logger mapping -> Edit based on column names 
 %A00 -> Pressure 
 %A01 -> UV
 %A02 -> Temperature 
@@ -58,21 +58,32 @@ depth1 = depth;
 depthdes = depth_des;
 
 
-%% converting to ACD counts to volts 
+% converting to ACD counts to volts 
 pressure_V = pressure_raw.*3.3/1024;
 uv_V = uv_raw.*3.3/1024;
 temp_V = temp_raw.*3.3/1024;
+
+%change:
+tdown = 1:1800; % _final = 2638:3060, 012 = , 002 = 3460:3980, 004 = 1290:1602
+
+% Separating data going down past our waypoints
+pressure_Vd = pressure_V(tdown);
+uv_Vd = uv_V(tdown);
+temp_Vd = temp_V(tdown);
+depthd = depth1(tdown);
 
 %% Calibrations 
 % pressure calibration: voltage -> depth 
 %depth = -2.9*pressure_V+8.88;
 
 %temperature calibration: voltage -> temp 
-temp_C = (-4.58*temp_V) + 23.2;
+temp_C = (-5.92*temp_V) + 25.5;
+temp_Cd = temp_C(tdown);
 
 %UV calibration: voltage -> UV index 
-G = 3.64 %Edit as needed 
+G = 8.085; %Edit as needed 
 uv_index = 10*(uv_V/G);
+uv_ind = uv_index(tdown);
 
 %% plots: depth vs voltages 
 figure;
@@ -80,55 +91,62 @@ plot(depth1,pressure_V,'.','Color','b');
 ylabel('Pressure Voltage (V)');
 xlabel('Depth (m)');
 title('Pressure Voltage vs Depth');
+legend('Dana Point, without motors');
 grid on;
 
 figure;
-plot(depth1,temp_V,'.','Color','r');
+plot(depthd,temp_Vd,'.','Color','r');
 ylabel('Temperature Voltage (V)');
 xlabel('Depth (m)');
 title('Temperature Voltage vs Depth')
+legend('Dana Point, without motors');
 grid on;
 
 figure;
-plot(depth1,uv_V,'.','Color','g');
+plot(depthd,uv_Vd,'.','Color','g');
 ylabel('UV Voltage (V)');
 xlabel('Depth (m)');
 title('UV Voltage vs Depth')
+legend('Dana Point, without motors');
 grid on;
 
 %% plot - depth vs all sensors 
 figure;
 hold on;
-plot(depth1, pressure_V,'.','Color','b','DisplayName','Pressure Voltage');
-plot(depth1, temp_V,'.','Color','r','DisplayName','Temperature Voltage');
-plot(depth1, uv_V,'.','Color','g','DisplayName','UV Voltage');
+plot(depthd, pressure_Vd,'.','Color','b','DisplayName','Pressure Voltage');
+plot(depthd, temp_Vd,'.','Color','r','DisplayName','Temperature Voltage');
+plot(depthd, uv_Vd,'.','Color','g','DisplayName','UV Voltage');
 
 ylabel('Voltage (V)');
 xlabel('Depth (m)');
-title('Sensor Voltages vs Depth')
+title('Sensor Voltages vs Depth at Dana Point, without motors')
 legend;
 grid on;
 
 
 %% Plots: depth vs calibrated values -> If wanted 
 figure;
-plot(depth1,temp_C,'.');
+plot(depthd,temp_Cd,'.');
 ylabel('Temperature (C)');
 xlabel('Depth (m)');
 title('Temperature vs Depth');
+legend('Dana Point, without motors');
 grid on;
 
 figure;
-plot(depth1,uv_index, '.');
+plot(depthd,uv_ind, '.');
 ylabel('UV Index');
 xlabel('Depth (m)');
 title('UV Index vs Depth');
+legend('Dana Point, without motors');
 grid on;
 
 figure;
 plot(depth1)
+title('Robot dive path at Dana Point, without motors');
 hold on;
 plot(depthdes)
+legend('Actual depth', 'Desired depth');
 hold off;
 
 figure;
@@ -140,9 +158,30 @@ xlim([5000,5951])
 
 figure;
 plot(temp_V);
+title('Temperature (V) vs time');
 
 figure;
 plot(uv_V);
+title('UV voltage vs time')
 
 figure;
 plot(temp_C);
+title('Temperature (C) vs time');
+
+figure;
+yyaxis left
+plot(depthd,uv_ind, '.');
+ylabel('UV Index');
+xlabel('Depth (m)');
+
+yyaxis right
+plot(depthd,temp_Cd,'.');
+ylabel('Temperature (C)');
+xlabel('Depth (m)');
+
+title('UV & Temperature vs Depth from Dana Point, run without motors');
+legend('UV', 'Temperature');
+grid on;
+
+%% poster graphs
+
